14922d1ffa9fce651e26e4d53f4f8699d6d57c59
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2025-10-05 08:10:41 +0700

    Add container with most water solution

---
+++ two_pointers/container_with_most_water.c
@@ -0,0 +1,29 @@
+// Source: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// Author: Ngoc-Tien To
+
+// Idea: Think of its as finding the largest rectangle formed by two lines.
+// Initialize two pointers (left, right) at the beginning and at the end.
+// The width decreases as we move any pointer,
+// so to maximum the area, we must look for a taller line,
+// by moving the pointer at shorter line inward
+
+int maxArea(int* height, int heightSize) {
+    int maxValue = 0;
+    int left = 0, right = heightSize - 1;
+
+    while(left < right){
+        int minHeight = (height[left] < height[right]) ? height[left] : height[right];
+        int area = (right - left) * minHeight;
+
+        maxValue = (area > maxValue) ? area : maxValue;
+
+        if(height[left] < height[right]){
+            left++;
+        }
+        else{
+            right--;
+        }
+    }    
+
+    return maxValue;
+}
