e545011a12e653abb65096f056073cd2e65ffe67
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2025-10-28 13:07:53 +0700

    Add: number of connected components solution

---
+++ graphs/number_of_connected_components_in_an_undirected_graph.cpp
@@ -0,0 +1,60 @@
+// -----------------------------------------------------------------------------
+// Problem: Number of Connected Components in an Undirected Graph
+// Source : https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+// Author : Ngoc-Tien To
+// -----------------------------------------------------------------------------
+
+// ---------------------------------- IDEA ----------------------------------
+//
+// 1. The problem asks for the number of connected components in an undirected graph.
+//
+// 2. Approach:
+//      - Build adjacency list from the edge list.
+//      - Use DFS (or BFS) to traverse each component.
+//      - Each time we start a DFS from an unvisited node, we found a new component.
+//
+// 3. Count how many times DFS starts -> that’s the number of components.
+//
+// Time Complexity  : O(n + e)
+// Space Complexity : O(n + e)
+// -----------------------------------------------------------------------------
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    // Depth-first search to visit all nodes in the same component
+    void dfs(const vector<vector<int>>& adj, int u, vector<bool>& visited) {
+        visited[u] = true;
+        for (auto& node : adj[u]) {
+            if (!visited[node]) {
+                dfs(adj, node, visited);
+            }
+        }
+    }
+
+    // Count the number of connected components in the graph
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        vector<bool> visited(n, false);
+
+        // Build adjacency list
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        int count = 0;
+
+        // Start DFS for each unvisited node
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(adj, i, visited);
+            }
+        }
+
+        return count;
+    }
+};
